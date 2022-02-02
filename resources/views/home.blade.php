f14d5741cf10c5011107a366a052624ae531ef49
Mostafa S. Amin <ms.amin@gmail.com>
2022-02-02 12:04:05 +0300

    UI

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,7 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="">
-                <div class=" d-flex justify-content-between">
+                <div class="d-flex justify-content-between">
                     <div>{{ __('NEW INVOICE') }}</div>
                     <div class="fw-light fs-6 mt-1">Total invoices: {{ $invoices_mini->total() }}</div>
                 </div>
@@ -18,28 +18,35 @@
                     @endif
 
                     <!-- <p><a href="{{ route('download-invoice') }}">Download Invoice</a></p> -->
-                    <table class="table table-bordered" id="dynamicTable">
-                        <tr class="bg-light text-dark h6">
-                            <th>&nbsp;</th>
-                            <th>Invoice Number</th>
-                            <th>Client Name</th>
-                            <th>Project Name</th>
-                            <th>Date</th>
-                            <th>&nbsp;</th>
-                            <th>&nbsp;</th>
-                        </tr>
-                        @foreach ( $invoices_mini as $key => $invoice_mini )
-                        <tr>
-                            <td>{{ $invoices_mini->firstItem() + $key }}</td>
-                            <td>EFC00{{ $invoice_mini->invoice_number }}</td>
-                            <td>{{ $invoice_mini->client_name }}</td>
-                            <td>{{ $invoice_mini->project_name }}</td>
-                            <td>{{ $invoice_mini->created_at }}</td>
-                            <td><button type="button" name="details" id="details" class="btn btn-primary">Details</button></td>
-                            <td><button type="button" name="export" id="export" class="btn btn-success">Export</button></td>
-                        </tr>
-                        @endforeach
-                    </table>
+                    <div class="table-responsive mb-3">
+                        <table class="table table-striped table-hover align-middle" id="dynamicTable">
+                            <!-- table-sm -->
+                            <thead class="table-secondary">
+                                <tr>
+                                    <th>#</th>
+                                    <th>Invoice Number</th>
+                                    <th>Client Name</th>
+                                    <th>Project Name</th>
+                                    <th>Date</th>
+                                    <th>&nbsp;</th>
+                                    <th>&nbsp;</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @foreach ( $invoices_mini as $key => $invoice_mini )
+                                <tr>
+                                    <td>{{ $invoices_mini->firstItem() + $key }}</td>
+                                    <td>EFC00{{ $invoice_mini->invoice_number }}</td>
+                                    <td>{{ $invoice_mini->client_name }}</td>
+                                    <td>{{ $invoice_mini->project_name }}</td>
+                                    <td>{{ $invoice_mini->created_at }}</td>
+                                    <td><button type="button" name="details" id="details" class="btn btn-primary btn-sm">Details</button></td>
+                                    <td><button type="button" name="export" id="export" class="btn btn-success btn-sm">Export</button></td>
+                                </tr>
+                                @endforeach
+                            </tbody>
+                        </table>
+                    </div>
 
                     {{-- Pagination --}}
                     <div class="d-flex justify-content-center">
@@ -59,8 +66,6 @@
                     </div>
                     @endif
 
-                    <!-- {{ __('You are logged in!') }} -->
-
                     <div class="input-group">
                         <input type="text" class="form-control" placeholder="{{Str::limit('Search by invoice number, client name', 20, '...')}}">
                         <span class="input-group-btn">
@@ -79,8 +84,6 @@
                         {{ session('status') }}
                     </div>
                     @endif
-
-                    <!-- {{ __('You are logged in!') }} -->
 
                     <!-- <div class="input-group">
                         <input type="text" class="form-control" placeholder="{{Str::words('Search by invoice number, client name', '3', '...')}}">
