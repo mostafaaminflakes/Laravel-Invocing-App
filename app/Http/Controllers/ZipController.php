232a163f0299739348f5ccf73537a18ec9b657bf
Mostafa S. Amin <ms.amin@gmail.com>
2022-02-14 15:30:50 +0300

    Zip Archive

--- app/Http/Controllers/ZipController.php
+++ app/Http/Controllers/ZipController.php
@@ -4,26 +4,58 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use ZipArchive;
-use File;
+use Illuminate\Support\Facades\Storage;
 
 class ZipController extends Controller
 {
     public function create()
     {
-        $zip_file_name = public_path('storage/invoices.zip');
         $zip = new ZipArchive;
+        $directory = 'invoices';
+        $zip_file_name = 'invoices.zip';
+        $zip_file_path = 'storage/' . $zip_file_name;
 
-        if ($zip->open($zip_file_name, ZipArchive::CREATE) === TRUE) {
-            $files = File::files(public_path('storage/invoices'));
+        // Delete old ZIP file if exists
+        $this->delete_file_if_exists($zip_file_name);
 
-            foreach ($files as $file) {
-                $relative_name_in_zip_file = basename($file);
-                $zip->addFile($file, $relative_name_in_zip_file);
+        // Create ZIP file
+        if ($zip->open(public_path($zip_file_path), ZipArchive::CREATE) === TRUE) {
+
+            // Create INVOICES directory if not exists
+            $this->create_directory_if_not_exists($directory);
+
+            // Add files inside directory to ZIP archive
+            $files = Storage::disk('public')->files($directory);
+
+            if (empty($files)) {
+                $zip->addEmptyDir($directory);
+            } else {
+                foreach ($files as $key => $file) {
+                    $relative_path = basename($file);
+                    $file_url = public_path('storage/' . $file);
+                    $zip->addFile($file_url, $relative_path);
+                }
             }
 
             $zip->close();
         }
 
-        return response()->download($zip_file_name);
+        // Stream ZIP file
+        return response()->download(public_path($zip_file_path));
+    }
+
+    public function delete_file_if_exists($file): void
+    {
+        if (Storage::disk('public')->exists($file)) {
+            Storage::disk('public')->delete($file);
+        }
+    }
+
+    public function create_directory_if_not_exists($directory): void
+    {
+        if (!Storage::disk('public')->exists($directory)) {
+            // File::makeDirectory($path, 0777, true, true);
+            Storage::disk('public')->makeDirectory($directory);
+        }
     }
 }
